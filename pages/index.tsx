57554299049a51d4f32582d35d565fc20ff83e57
Evdokia Bartzi <eydokiampartzi@gmail.com>
2020-11-07 10:00:32 +0200

    Creted static generation for index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
-import { useEffect, useState } from 'react';
-
 import Trending from '../components/trending/Trending';
 import request from '../modules/request';
-import { AppWrapper, GlobalStyle } from '../styles/index.styles';
+import { AppWrapper, GlobalStyle, Navigation } from '../styles/index.styles';
 
-const Home = (): JSX.Element => {
-  const [data, setData] = useState([]);
+interface Community {
+  title: string;
+  url: string;
+  members: number;
+  image: string;
+}
 
-  useEffect(() => {
-    const queryData = async () => {
-      const query = `{
+interface HomeProps {
+  communities: Community[];
+}
+const Home = ({ communities }: HomeProps): JSX.Element => {
+  return (
+    <AppWrapper>
+      <GlobalStyle />
+      <Trending communities={communities} />
+      <Navigation href="/addCommunity">Add Community</Navigation>
+    </AppWrapper>
+  );
+};
+
+// This function gets called at build time
+export const getStaticProps = async (): Promise<unknown> => {
+  // Call an external API endpoint to get communities
+
+  const query = `{
         get {
           title
           url
@@ -18,18 +35,15 @@ const Home = (): JSX.Element => {
         }
       }`;
 
-      const response = await request(query);
-      setData(response);
-    };
-    queryData();
-  }, []);
+  const communities = await request(query);
 
-  return (
-    <AppWrapper>
-      <GlobalStyle />
-      <Trending communities={data} />
-    </AppWrapper>
-  );
+  // By returning { communities: communities }, the Home component
+  // will receive `communities` as a prop at build time
+  return {
+    props: {
+      communities
+    }
+  };
 };
 
 export default Home;
