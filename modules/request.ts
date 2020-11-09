875f59c1b8fac46db391524333f228923a6bf1d4
Evdokia Bartzi <eydokiampartzi@gmail.com>
2020-11-09 13:48:17 +0200

    Request changed try,catch added.

--- modules/request.ts
+++ modules/request.ts
@@ -1,20 +1,24 @@
 const request = async (query: string): Promise<unknown> => {
-  const spreadsheetId = '1HbGGv7ihEW7uHkU4znlLL8FwJPBb-wQ3CFZlYfUtbDk';
-  const response = await fetch(
-    `https://api.graphqlsheet.com/api/${spreadsheetId}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        token: '1a28a8ce133c440d00d3e4d2081b05aa66b642af'
-      },
-      body: JSON.stringify({
-        query
-      })
-    }
-  );
-  const responseJSON = await response.json();
-  return responseJSON.data.get;
+  try {
+    const spreadsheetId = '1HbGGv7ihEW7uHkU4znlLL8FwJPBb-wQ3CFZlYfUtbDk';
+    const response = await fetch(
+      `https://api.graphqlsheet.com/api/${spreadsheetId}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          token: '1a28a8ce133c440d00d3e4d2081b05aa66b642af'
+        },
+        body: JSON.stringify({
+          query
+        })
+      }
+    );
+    const responseJSON = await response.json();
+    return responseJSON.data;
+  } catch (error) {
+    alert('Error, try again!');
+  }
 };
 
 export default request;
