c47d618529d659879110af6ad647877ce97154de
Evdokia Bartzi <eydokiampartzi@gmail.com>
2020-11-11 11:02:29 +0200

    Request type changed

--- modules/request.ts
+++ modules/request.ts
@@ -1,4 +1,4 @@
-const request = async (query: string): Promise<unknown> => {
+const request = async (query: string): Promise<T> => {
   try {
     const spreadsheetId = '1HbGGv7ihEW7uHkU4znlLL8FwJPBb-wQ3CFZlYfUtbDk';
     const response = await fetch(
