16d91a377e50fd19371f1be692475bf8248486e4
Evdokia Bartzi <eydokiampartzi@gmail.com>
2020-11-16 14:16:09 +0200

    Trending component  updated

--- components/trending/Trending.tsx
+++ components/trending/Trending.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import { CommunityType } from '../../modules/types';
@@ -16,21 +17,27 @@ export interface TrendingProps {
   communities: CommunityType[];
 }
 
-const Trending = ({ communities }: TrendingProps): JSX.Element => (
-  <Card title="Trending Communities">
-    {communities.map(({ title, url, members, image }) => (
-      <Community key={title}>
-        <Details>
-          <Image src={image} />
-          <Info>
-            <CommunityTitle href={url}>{title}</CommunityTitle>
-            <Members>{members} members</Members>
-          </Info>
-        </Details>
-        <Button text="JOIN" />
-      </Community>
-    ))}
-  </Card>
-);
+const Trending = ({ communities }: TrendingProps): JSX.Element => {
+  const router = useRouter();
 
+  return (
+    <Card title="Trending Communities">
+      {communities.map(({ title, url, members, image }) => (
+        <Community key={title}>
+          <Details>
+            <Image src={image} />
+            <Info>
+              <CommunityTitle href={url}>{title}</CommunityTitle>
+              <Members>{members} members</Members>
+            </Info>
+          </Details>
+          <Button
+            text="JOIN"
+            action={() => router.push(`/community/${title}`)}
+          />
+        </Community>
+      ))}
+    </Card>
+  );
+};
 export default Trending;
